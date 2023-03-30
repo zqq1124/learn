4859a029719fdf6392d2ce3ff5468c37b7516b36
zqq <1934386406@qq.com>
2023-03-30 16:41:47 +0800

    First Commit

---
+++ learn gitdemo/learn gitdemo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learn gitdemo
+//
+//  Created by zhangqq on 2023/3/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
